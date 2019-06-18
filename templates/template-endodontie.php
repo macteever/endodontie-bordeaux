c72e7e1ca0471f908f4bbea1d97f80eb63890eb6
nmetivier <contact@nicolasmetivier.fr>
2019-06-18 13:01:42 +0200

    fin page endodontiq

--- templates/template-endodontie.php
+++ templates/template-endodontie.php
@@ -1,107 +1,180 @@
 <?php /* Template Name: Endodontie */ get_header(); ?>
 <main role="main" class="main-content">
-   <?php
-      if( have_rows('top_page') ):
-          while ( have_rows('top_page') ) : the_row();
-          ?>
-          <div class="container-fluid redac-top-page" style="background: url(<?php the_sub_field('bkg'); ?>); background-size: cover; background-position: bottom;">
-             <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                   <div class="offset-xl-1 offset-lg-1 col-xl-6 col-lg-7 col-md-12 col-12 pl-0 pb-30 page-intro anim-700">
-                     <div class="page-intro-anim anim-700">
-                        <h1 class="montserrat fs-72 fw-700 mt-0 ml-30 anim-700"><?php the_sub_field('title'); ?></h1>
-                        <div class="montserrat fs-18 lh-24 mb-50 ml-30 anim-700">
-                           <?php the_sub_field('headline'); ?>
-                        </div>
-                        <div class="ml-30 anim-700">
-                           <a class="btn-blue text-white" href="<?php the_sub_field('lien'); ?>">Me contacter</a>
-                        </div>
-                     </div>
-                   </div>
+  <section id="endo-top">
+    <?php
+    if( have_rows('endo_top') ):
+      while ( have_rows('endo_top') ) : the_row();
+      ?>
+        <div class="container-fluid" style="background: url(<?php the_sub_field('bkg_part') ?>); background-size: contain; background-repeat: no-repeat;">
+        <div class="endo-filter mw-60"></div>
+          <div class="container">
+            <div class="row align-items-center mw-100">
+              <div class="col-xl-9 col-lg-9 col-md-9 col-12 d-flex flex-column ml-auto">
+                <div class="content-subtitle fs-22 text-right">
+                  <?php the_sub_field('subtitle'); ?>
                 </div>
-             </div>
+                <div class="content-title montserrat fs-80 text-blue mb-15 fw-700 text-right">
+                  <?php the_sub_field('title'); ?>
+                </div>
+                <div class="home-excerpt fs-17 lh-28 mt-15 mb-15 mw-50 ml-auto text-right">
+                  <?php the_sub_field('excerpt'); ?>
+                </div>
+              </div>
+            </div>
           </div>
+          <div class="box-parent d-flex flex-column align-items-center">
+            <p class="fs-12 text-center ls-2 mb-10">SCROLL</p>
+            <div class="box">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+    <?php
+    endwhile;
+    else :
+    endif;
+    ?>
+  </section>
+  <section id="endo-about" class="container-fluid">
+    <div class="container">
+      <div class="row mb-50">
+        <div class="col-12 fs-52 text-blue">
+          <?php the_field('endo_subtitle'); ?>
+        </div>
+      </div>
       <?php
-      endwhile;
+        if ( have_rows('endo_about') ): ?>
+          <?php while ( have_rows('endo_about') ) : the_row();
+          ?>
+          <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-12">
+              <?php
+                if ( have_rows('endo_about_content') ): ?>
+                  <?php while ( have_rows('endo_about_content') ) : the_row();
+                  ?>
+                  <div class="endo-about-content">
+                    <?php the_sub_field('content'); ?>
+                  </div>
+                  <?php endwhile;
+                 endif;
+               ?>
+               <div class="mt-50">
+                 <?php
+                 $link = get_sub_field('link');
+                 if( $link ):
+                   $link_url = $link['url'];
+                   $link_title = $link['title'];
+                   ?>
+                   <a class="fs-20 text-blue fw-700" href="<?php echo esc_url($link_url); ?>"><?php echo esc_html($link_title); ?></a>
+                 <?php endif; ?>
+               </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-12 text-center">
+              <?php
+              $image = get_sub_field('image');
+              if ( !empty($image) ): ?>
+                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+              <?php endif; ?>
+              <p class="fs-15"><?php the_sub_field('legende'); ?></p>
+            </div>
+          </div>
+        <?php endwhile;
+       endif;
+       ?>
+    </div>
+  </section>
+  <section id="endo-consult" class="container-fluid">
+    <div class="container">
+    <?php
+    if( have_rows('endo_consult') ):
+      while ( have_rows('endo_consult') ) : the_row();
+      ?>
+     <div class="row align-items-end justify-content-between">
+       <div class="col-xl-6 col-lg-6 col-md-12 col-12">
+         <div class="fs-52 text-white mb-30">
+           <?php the_sub_field('title'); ?>
+         </div>
+         <div class="enbdo-consult-content">
+           <div class="fs-30 text-white mb-30">
+             <?php the_sub_field('subtitle'); ?>
+           </div>
+           <div class="fs-17 lh-26 text-white">
+             <?php the_sub_field('content'); ?>
+           </div>
+           <div class="mt-30 text-white">
+             <?php
+              $link = get_sub_field('link');
+              if( $link ):
+                $link_url = $link['url'];
+                $link_title = $link['title'];
+                ?>
+                <a class="fw-700 fs-18 text-white" href="<?php echo esc_url($link_url); ?>"><?php echo esc_html($link_title); ?></a>
+              <?php endif; ?>
+           </div>
+         </div>
+       </div>
+       <div class="col-xl-6 col-lg-6 col-md-12 col-12 p-relative text-center">
+         <?php
+         if( have_rows('endo_consult_img') ):
+           while ( have_rows('endo_consult_img') ) : the_row();
+           ?>
+           <?php
+             $image = get_sub_field('image');
+             if( !empty($image) ): ?>
+               <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" description="<?php echo $image['description']; ?>"/>
+             <?php endif;
+           ?>
+           <?php
+           endwhile;
+         else :
+         endif;
+         ?>
+       </div>
+     </div>
+     <?php
+     endwhile;
+     else :
+     endif;
+     ?>
+    </div>
+  </section>
+  <section id="endo-traitement" class="container-fluid">
+    <div class="container">
+      <?php
+      if( have_rows('endo_traitement') ):
+        while ( have_rows('endo_traitement') ) : the_row();
+        ?>
+        <div class="row justify-content-around">
+          <div class="col-12 text-blue fs-52 mb-50">
+            <?php the_sub_field('title'); ?>
+          </div>
+          <div class="col-xl-5 col-lg-5 col-12 fs-17 lh-26">
+            <?php the_sub_field('content'); ?>
+          </div>
+          <div class="col-xl-5 col-lg-5 col-12">
+            <?php the_sub_field('content_list'); ?>
+          </div>
+        </div>
+        <?php
+        endwhile;
       else :
       endif;
       ?>
-      <div class="container-fluid">
-         <div class="container">
-            <?php
-            if( have_rows('redac_content') ):
-               while ( have_rows('redac_content') ) : the_row();
-               ?>
-               <div class="row mt-15 mb-150 redac-content align-items-end justify-content-around">
-                  <div class="col-xl-6 col-lg-6 col-md-12 col-12 zi-9 redac-content-img">
-                     <?php
-                        $image = get_sub_field('img');
-                     ?>
-                     <?php
-                     echo '<pre>';
-                    	var_dump( $image );
-                     echo '</pre>';
-                     ?>
-                     <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" title="<?php echo $image['title']; ?>" />
-                  </div>
-                  <div class="col-xl-6 col-lg-6 col-md-12 col-12 zi-99 pl-30">
-                     <div class="montserrat fs-66 fw-700 mb-30"><?php the_sub_field('subtitle'); ?></div>
-                     <div class="montserrat fs-17 lh-24 mt-20 ">
-                        <?php the_sub_field('content'); ?>
-                     </div>
-                  </div>
-               </div>
-               <?php
-               endwhile;
-               else :
-               endif;
-               ?>
-            <div class="row justify-content-around mb-150">
-               <?php
-               if( have_rows('redac_textbox') ):
-                  while ( have_rows('redac_textbox') ) : the_row();
-                  ?>
-               <div class="col-xl-5 col-lg-5 col-md-12 col-12 d-flex redac-textbox align-items-center p-15">
-                  <div>
-                     <?php the_sub_field('icon'); ?>
-                  </div>
-                  <div class="montserrat text-darkgrey fs-22 lh-28 ml-30">
-                     <?php the_sub_field('message'); ?>
-                  </div>
-               </div>
-               <?php
-               endwhile;
-               else :
-               endif;
-               ?>
-            </div>
-         </div>
+      <div class="row mt-80">
+        <div class="col-12 text-center">
+          <?php
+           $link = get_field('endo_rdv');
+           if( $link ):
+             $link_url = $link['url'];
+             $link_title = $link['title'];
+             ?>
+             <a class="btn-blue" href="<?php echo esc_url($link_url); ?>"><?php echo esc_html($link_title); ?></a>
+           <?php endif; ?>
+        </div>
       </div>
-      <div class="container-fluid redac-banner mb-150" style="background: url(<?php the_field('redac_banner'); ?>);">
-      </div>
-      <div class="container-fluid mt-50 mb-150">
-         <div class="container">
-         <?php
-         if( have_rows('redac_endpage') ):
-            while ( have_rows('redac_endpage') ) : the_row();
-            ?>
-            <div class="row justify-content-center redac-endpage">
-               <div class="col-xl-8 col-lg-10 col-md-12 col-12 mb-50">
-                  <div class="montserrat fs-72 fw-700 text-center mb-50">
-                     <?php the_sub_field('subtitle'); ?>
-                  </div>
-                  <div class="montserrat fs-18 text-center">
-                     <?php the_sub_field('text'); ?>
-                  </div>
-               </div>
-            </div>
-            <?php
-            endwhile;
-            else :
-            endif;
-            ?>
-         </div>
-      </div>
+    </div>
+  </section>
 </main>
 <!-- /container-fluid -->
 <?php get_footer(); ?>
